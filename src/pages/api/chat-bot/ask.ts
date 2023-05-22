b67464aecc610723c6b01765a05c11ef98d07fd3
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2023-05-22 21:40:32 +0200

    Add ask endpoint new logic to accept  other user intention

--- src/pages/api/chat-bot/ask.ts
+++ src/pages/api/chat-bot/ask.ts
@@ -34,85 +34,88 @@ export default async function handler (
         return
       }
 
-      if (data.body.country != null && (data.body.province == null || data.body.province == null) && !(await checkIfValidCountry(data.body.country))) {
-        const country = await getCountryName(data.body.country)
+      switch (data.responseType) {
+        case IntentionType.OFFER_SEARCH:
+          // TODO Encapsulate this function
+          if (data.body.country != null && (data.body.province == null || data.body.province == null) && !(await checkIfValidCountry(data.body.country))) {
+            const country = await getCountryName(data.body.country)
 
-        if (country == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (country == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        const { key } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { key: null }
+            const { key } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { key: null }
 
-        if (key == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (key == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        data.body.country = key
-      } else if (data.body.province != null && data.body.city == null && !(await checkIfValidProvince(data.body.province))) {
-        const country = await getCountryName(data.body.province)
+            data.body.country = key
+          } else if (data.body.province != null && data.body.city == null && !(await checkIfValidProvince(data.body.province))) {
+            const country = await getCountryName(data.body.province)
 
-        if (country == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (country == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        const { id } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { id: null }
-        const provinceList = await getDictionaryList('province', String(id))
+            const { id } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { id: null }
+            const provinceList = await getDictionaryList('province', String(id))
 
-        if (provinceList == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (provinceList == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        const validProvince = await getCorrectProvince(data.body.province, provinceList.map(p => p.key).join(','))
+            const validProvince = await getCorrectProvince(data.body.province, provinceList.map(p => p.key).join(','))
 
-        if (validProvince == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (validProvince == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        data.body.province = validProvince
-      } else if (data.body.city != null && !(await checkIfValidCity(data.body.city))) {
-        const country = await getCountryName(data.body.city)
+            data.body.province = validProvince
+          } else if (data.body.city != null && !(await checkIfValidCity(data.body.city))) {
+            const country = await getCountryName(data.body.city)
 
-        if (country == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (country == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        const { id } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { id: null }
-        const cityList = await getDictionaryList('city', String(id))
+            const { id } = (await getDictionaryList('country'))?.find(d => d.value.toLowerCase() === country.toLowerCase()) ?? { id: null }
+            const cityList = await getDictionaryList('city', String(id))
 
-        if (cityList == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (cityList == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        const validCity = await getCorrectCity(data.body.city, cityList.map(p => p.key).join(','))
+            const validCity = await getCorrectCity(data.body.city, cityList.map(p => p.key).join(','))
 
-        if (validCity == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
+            if (validCity == null) {
+              res.status(500).json({ message: 'Internal server error' })
+              return
+            }
 
-        data.body.city = validCity
-        delete data.body.province
+            data.body.city = validCity
+            delete data.body.province
+          }
+
+          const offers = await getOffers(data.body)
+
+          if (offers == null) {
+            res.status(500).json({ message: 'Internal server error' })
+            return
+          }
+
+          return res.json({ message: 'Success', offers, messageRole: MessageRole.BOT, createdAt: new Date(), responseType: IntentionType.OFFER_SEARCH })
+        case IntentionType.INTRODUCTION:
+          return res.json({ message: 'Success', text: data.body as string, messageRole: MessageRole.BOT, createdAt: new Date(), responseType: IntentionType.INTRODUCTION })
       }
 
-      if (data.responseType === IntentionType.OFFER_SEARCH) {
-        const offers = await getOffers(data.body)
-
-        if (offers == null) {
-          res.status(500).json({ message: 'Internal server error' })
-          return
-        }
-
-        return res.json({ message: 'Success', offers, messageRole: MessageRole.BOT, createdAt: new Date(), responseType: IntentionType.OFFER_SEARCH })
-      }
-
-      console.log(intention)
       break
     default:
       res.setHeader('Allow', ['POST'])
