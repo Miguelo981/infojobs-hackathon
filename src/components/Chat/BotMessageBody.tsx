7325e13e37f9f4a235a17a97a0828d365d63ecb2
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2023-05-22 21:33:55 +0200

    Add chat design

--- src/components/Chat/BotMessageBody.tsx
+++ src/components/Chat/BotMessageBody.tsx
@@ -7,21 +7,30 @@ interface BotMessageBodyProps {
 
 export default function BotMessageBody ({ message }: BotMessageBodyProps) {
   return (
-    <>
+    <div className='flex gap-x-2 items-start'>
+      {/* <ChatBotIcon className='w-16 h-auto' /> */}
       {
       message.responseType === IntentionType.OFFER_SEARCH
-        ? <div className='flex flex-col gap-3'>
-          {
-              message.offers?.items?.map((item) => (
-                <div key={item.id} className='px-4 py-2 bg-blue-300 text-black w-fit rounded-r-lg rounded-tl-lg'>
-                  <MinimumOfferDetail item={item} />
+        ? (
+          <div className='flex flex-col gap-3'>
+            {
+              message.offers?.items?.map((item, index: number) => (
+                <div key={item.id} className='px-4 py-2 bg-secondary text-primary w-fit rounded-r-lg rounded-tl-lg'>
+                  <MinimumOfferDetail item={item} index={index + 1} />
                 </div>
               ))
             }
           </div>
+          )
 
-        : null
+        : message.responseType === IntentionType.INTRODUCTION
+          ? (
+            <div className='px-4 py-2 bg-secondary text-primary w-fit rounded-r-lg rounded-tl-lg'>
+              <p>{message.text}</p>
+            </div>
+            )
+          : null
       }
-    </>
+    </div>
   )
 }
--- src/components/Chat/MinimumOfferDetail.tsx
+++ src/components/Chat/MinimumOfferDetail.tsx
@@ -2,12 +2,14 @@ import { Item } from '@/models/infojobs/offer'
 
 interface MinimumOfferDetailProps {
   item: Item
+  index: number
 }
 
-export default function MinimumOfferDetail ({ item }: MinimumOfferDetailProps) {
+export default function MinimumOfferDetail ({ item, index }: MinimumOfferDetailProps) {
   return (
-    <article className='text-black'>
-      <h2>{item.title}</h2>
+    <article className='text-inherit'>
+      <h2><small>{index}</small>. {item.title}</h2>
+      <small>{item.salaryMin.value}</small>
     </article>
   )
 }
