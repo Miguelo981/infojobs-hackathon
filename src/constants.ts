5b3e056d7825e6716ca02ac5b00eb8d8935fabbb
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2023-05-23 14:04:58 +0200

    Add constants list

--- src/constants.ts
+++ src/constants.ts
@@ -21,9 +21,14 @@ export const OPENAI_REQUEST_INTENTION_SCHEMA = `
     sinceDate?: string
     teleworking?: string // accepts 'teletrabajo-posible' or 'solo-teletrabajo'
   }
-  provinces are lowercase always
+  provinces are lowercase always,
+  If the user wants to know more about the chat bot usage, options, the responseType is 1 and the body will be the an string talking about what you offer yourself in Spanish,
+  If the user says hello or introduce himself, the responseType is 1 and the body will be the an string introducing talking about what you offer yourself in Spanish,
+  If the user wants to know more about a job offer, the responseType is 2 and the body will be the empty object,
+  If the user wants to postulate to a job offer, the responseType is 3 and the body will be the empty object,
+  
 `
-export const OPENAI_RESPONSE_FORMAT = `The format must be in JSON like this: 
+export const OPENAI_RESPONSE_FORMAT = `Do not reply anything else but the following return format in JSON like this: 
 {
   "responseType": [[type]],
   "body": [[body]]
@@ -38,6 +43,27 @@ export const TEST_OPENAI_RESPONSE = `
       "contry": "espana",
       "q": "programador web",
     }
+  }
+`
+export const TEST_OPENAI_REQUEST2 = 'Buenas tardes'
+export const TEST_OPENAI_RESPONSE2 = `
+  {
+    "responseType": 1,
+    "body": "Soy tu Asistente de Infojobs, puedes preguntarme sobre ofertas de trabajo."
+  }
+`
+export const TEST_OPENAI_REQUEST3 = 'Me gustaria saber mas sobre la oferta 3'
+export const TEST_OPENAI_RESPONSE3 = `
+  {
+    "responseType": 2,
+    "body": {}
+  }
+`
+export const TEST_OPENAI_REQUEST4 = 'Quiero postularme a la oferta'
+export const TEST_OPENAI_RESPONSE4 = `
+  {
+    "responseType": 3,
+    "body": {}
   }
 `
 export const CHAT_MESSAGES_LS_KEY = 'chat-messages'
